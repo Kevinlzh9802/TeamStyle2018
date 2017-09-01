b3892ca619a56573d532b4eba153320bfc6e0c3a
AlbertHuyb <13935047516@163.com>
2017-09-01 18:57:52 +0800

    addhtml

--- cplash/mainsite/views.py
+++ cplash/mainsite/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+	template = get_template('home.html')
+	html = template.render(locals())
+
+	return HttpResponse(html)
